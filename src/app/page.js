86550b1e8464c7805eacee0909af0c2d749dfc75
festus <festusishioma123@gmail.com>
2023-12-27 13:16:23 +0100

    redesigned mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,30 @@
 
 import styles from "./page.module.css"
+import Image from "next/image"
 export default function Home(){
   return(
     <div id={styles["container"]}>
-      <video autoPlay muted loop id={styles["banner"]}>
+
+
+      {/* desktop */}
+      <div id={styles["desktop_wrapper"]}>
+      <video autoPlay muted loop id={styles["desktop_banner"]}>
        <source src="/videos/banner.mp4" type="video/mp4" />
        </video>
-       
+       </div>
        <div id={styles["content"]}>
           <h1>Hello world</h1>
+       </div>
+
+
+       {/* mobile */}
+       <div id={styles["mobile_banner"]}>
+         <Image src="/image/img.jpg" alt="mobile banner"
+           width={0}
+           height={0}
+           sizes="100vw"
+           style={{ width: '100%', height: 'auto' }} 
+         />
        </div>
     </div>
     // <Upload/>
