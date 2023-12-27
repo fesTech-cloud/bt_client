6b5c0b50721bfb9445159ea8f5fe6db4da20f2d7
festus <festusishioma123@gmail.com>
2023-12-27 14:24:59 +0100

    added reacting typing to mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,47 @@
+"use client"
 
 import styles from "./page.module.css"
+
 import Image from "next/image"
+import { TypeAnimation } from 'react-type-animation';
 export default function Home(){
+
+  
   return(
     <div id={styles["container"]}>
-
 
       {/* desktop */}
       <div id={styles["desktop_wrapper"]}>
       <video autoPlay muted loop id={styles["desktop_banner"]}>
        <source src="/videos/banner.mp4" type="video/mp4" />
        </video>
-       </div>
-       <div id={styles["content"]}>
+       <div className={styles["content"]}>
           <h1>Hello world</h1>
        </div>
 
+       </div>
+      
 
        {/* mobile */}
        <div id={styles["mobile_banner"]}>
-         <Image src="/image/img.jpg" alt="mobile banner"
-           width={0}
-           height={0}
-           sizes="100vw"
-           style={{ width: '100%', height: 'auto' }} 
-         />
+         <div className={styles["content"]}>
+         <h3>Welcome to</h3>
+              <h2>
+              <TypeAnimation
+               sequence={[
+               // Same substring at the start will only be typed out once, initially
+               'Breakthrough Parish',
+               1000, // wait 1s before replacing "Mice" with "Hamsters"
+               'A home Of Worshippers',
+           1000]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+              </h2>
+              
+         </div>
        </div>
     </div>
     // <Upload/>
